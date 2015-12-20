45d418b91ebd66142f451a973e4702cfddc3900e
alexis <alexis.legoff@gmail.com>
2015-12-20 14:27:11 +0100

    add stop-ui script + disable lock screen by default

--- device.mk
+++ device.mk
@@ -23,6 +23,7 @@ PRODUCT_COPY_FILES := \
     device/asus/tilapia/fstab.grouper:root/fstab.grouper \
     device/asus/tilapia/init.tilapia.rc:root/init.grouper.rc \
     device/asus/tilapia/familinklogo.rle:root/initlogo.rle \
+    device/asus/tilapia/stop-ui.sh:system/etc/stop-ui.sh \
     device/asus/tilapia/init.framapia.sh:system/etc/init.framapia.sh \
     device/asus/tilapia/bootanimation.zip:system/media/bootanimation.zip \
     device/asus/tilapia/apns-conf.xml:system/etc/apns-conf.xml \
